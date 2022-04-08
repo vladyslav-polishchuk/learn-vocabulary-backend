27e102145311778429c376bbcdc7839c126bd9b5
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-04-08 10:32:02 +0300

    Implement initial version of DB schema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn-vocabulary-backend
+
 Backend part of application to extend English vocabulary
+
+Current version of DB schema
+https://lucid.app/lucidchart/e84f727c-5812-4738-a273-554b5ce05b82/edit?beaconFlowId=1C974460D4032DA8&page=0_0&invitationId=inv_e3ee550f-1d69-4fcc-88c4-609ec637d6ce#
--- src/db/createSchema.ts
+++ src/db/createSchema.ts
@@ -1,10 +1,29 @@
 import { Database } from 'sqlite3';
 
 const initSQLQuery = `
-	CREATE TABLE IF NOT EXISTS projects (
+	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT
-	)
+	);
+
+	CREATE TABLE IF NOT EXISTS words (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		text TEXT,
+		frequency INTEGER
+	);
+
+	CREATE TABLE IF NOT EXISTS processed_books (
+		hash TEXT PRIMARY KEY,
+		name TEXT,
+		share BOOLEAN,
+		user INTEGER
+	);
+
+	CREATE TABLE IF NOT EXISTS learned_words (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user INTEGER,
+		word INTEGER
+	);
 `;
 
 export default async function createSchema(db: Database) {
