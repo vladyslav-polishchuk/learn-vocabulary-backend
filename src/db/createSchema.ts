f13e1b2a743f198635c7414b032a11e281a4f996
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-04-09 15:28:04 +0300

    Fix issue when only single table is created on app startup

--- src/db/createSchema.ts
+++ src/db/createSchema.ts
@@ -1,41 +1,43 @@
 import { Database } from 'sqlite3';
 
-const initSQLQuery = `
-	CREATE TABLE IF NOT EXISTS users (
+const tables = [
+  `CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT
-	);
-
-	CREATE TABLE IF NOT EXISTS words (
+	);`,
+  `CREATE TABLE IF NOT EXISTS words (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		text TEXT,
 		frequency INTEGER
-	);
-
-	CREATE TABLE IF NOT EXISTS processed_books (
+	);`,
+  `CREATE TABLE IF NOT EXISTS processed_books (
 		hash TEXT PRIMARY KEY,
 		name TEXT,
 		share BOOLEAN,
 		user INTEGER
-	);
-
-	CREATE TABLE IF NOT EXISTS learned_words (
+	);`,
+  `CREATE TABLE IF NOT EXISTS learned_words (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		user INTEGER,
 		word INTEGER
-	);
-`;
+	);`,
+];
 
 export default async function createSchema(db: Database) {
-  return new Promise((resolve, reject) => {
-    db.run(initSQLQuery, [], function (err) {
-      if (err) {
-        console.log('Error running setup sql');
-        console.log(err);
-        reject(err);
-      } else {
-        resolve(true);
-      }
-    });
-  });
+  const createTablePromises = tables.map(
+    (createTableSql) =>
+      new Promise((resolve, reject) => {
+        db.run(createTableSql, [], (err) => {
+          if (err) {
+            console.log('Error running setup sql');
+            console.log(err);
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        });
+      })
+  );
+
+  return Promise.all(createTablePromises);
 }
