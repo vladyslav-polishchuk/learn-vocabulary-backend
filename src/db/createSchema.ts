2cb5e654a642790d509e1b419c1fe04836e6ef5b
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-04-16 13:26:17 +0300

    Create DBSchema within single transaction

--- src/db/createSchema.ts
+++ src/db/createSchema.ts
@@ -1,43 +1,38 @@
 import { Database } from 'sqlite3';
 
-const tables = [
-  `CREATE TABLE IF NOT EXISTS users (
+const setupSql = `
+  CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT
-	);`,
-  `CREATE TABLE IF NOT EXISTS words (
+	);
+  CREATE TABLE IF NOT EXISTS words (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 		value TEXT,
 		count INTEGER
-	);`,
-  `CREATE TABLE IF NOT EXISTS books (
+	);
+  CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 		hash TEXT,
 		name TEXT,
 		share BOOLEAN,
 		user INTEGER
-	);`,
-  `CREATE TABLE IF NOT EXISTS learned_words (
+	);
+  CREATE TABLE IF NOT EXISTS learned_words (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		user INTEGER,
 		word INTEGER
-	);`,
-];
+	);
+`;
 
 export default async function createSchema(db: Database) {
-  const createTablePromises = tables.map(
-    (createTableSql) =>
-      new Promise((resolve, reject) => {
-        db.run(createTableSql, [], (err) => {
-          if (err) {
-            console.log('Error running setup sql', err);
-            reject(err);
-          } else {
-            resolve(true);
-          }
-        });
-      })
-  );
-
-  return Promise.all(createTablePromises);
+  return new Promise((resolve, reject) => {
+    db.exec(setupSql, (err) => {
+      if (err) {
+        console.log('Error running setup sql', err);
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    });
+  });
 }
