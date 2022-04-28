8ed7d432f44fea7667f9c0c5ca309b70fc0097ef
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2022-04-28 15:53:13 +0300

    Protect book POST route so that only logged user can upload books

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -5,6 +5,7 @@ import type DataAccessLayer from '../db/DataAccessLayer';
 import type { Request, Response } from 'express';
 import type { UploadedFile } from 'express-fileupload';
 import type { DbRecord } from '../db/DataAccessLayer';
+import { verifyToken } from './auth';
 
 export default function (dataAccessLayer: DataAccessLayer) {
   const router = express.Router();
@@ -42,58 +43,65 @@ export default function (dataAccessLayer: DataAccessLayer) {
     });
   });
 
-  router.post('/book', async function (request: Request, response: Response) {
-    const bookFile = request.files.book as UploadedFile;
-    const words = await getWordsSortedByFrequency(bookFile.data, bookFile.name);
-    const bookInDb = await dataAccessLayer.read('books', {
-      hash: bookFile.md5,
-    });
-    const book = {
-      hash: bookFile.md5,
-      name: bookFile.name,
-      share: false,
-    };
+  router.post(
+    '/book',
+    verifyToken,
+    async function (request: Request, response: Response) {
+      const bookFile = request.files.book as UploadedFile;
+      const words = await getWordsSortedByFrequency(
+        bookFile.data,
+        bookFile.name
+      );
+      const bookInDb = await dataAccessLayer.read('books', {
+        hash: bookFile.md5,
+      });
+      const book = {
+        hash: bookFile.md5,
+        name: bookFile.name,
+        share: false,
+      };
 
-    if (bookInDb.length) {
-      response.send({ words, ...book });
-      return;
-    }
-
-    const fileDirectory = `./uploads/${bookFile.md5}`;
-    fs.mkdir(fileDirectory, { recursive: true }, async (err) => {
-      if (err) {
-        console.log('File was not saved', err);
-      } else {
-        await bookFile.mv(`${fileDirectory}/${bookFile.name}`);
+      if (bookInDb.length) {
+        response.send({ words, ...book });
+        return;
       }
-    });
 
-    await dataAccessLayer.create('books', book);
-
-    const wordsFromDb = await dataAccessLayer.read('words');
-    const wordKeyByValue = new Map();
-    wordsFromDb.forEach((word) => {
-      wordKeyByValue.set(word.value, word);
-    });
-
-    await dataAccessLayer.transaction(async () => {
-      for (const word of words) {
-        const wordFromDb = wordKeyByValue.get(word.value);
-        if (!wordFromDb) {
-          await dataAccessLayer.create('words', word as unknown as DbRecord);
-          continue;
+      const fileDirectory = `./uploads/${bookFile.md5}`;
+      fs.mkdir(fileDirectory, { recursive: true }, async (err) => {
+        if (err) {
+          console.log('File was not saved', err);
+        } else {
+          await bookFile.mv(`${fileDirectory}/${bookFile.name}`);
         }
+      });
 
-        const count = wordFromDb.count + word.count;
-        await dataAccessLayer.update('words', {
-          ...wordFromDb,
-          count,
-        });
-      }
-    });
+      await dataAccessLayer.create('books', book);
 
-    response.send({ words, ...book });
-  });
+      const wordsFromDb = await dataAccessLayer.read('words');
+      const wordKeyByValue = new Map();
+      wordsFromDb.forEach((word) => {
+        wordKeyByValue.set(word.value, word);
+      });
+
+      await dataAccessLayer.transaction(async () => {
+        for (const word of words) {
+          const wordFromDb = wordKeyByValue.get(word.value);
+          if (!wordFromDb) {
+            await dataAccessLayer.create('words', word as unknown as DbRecord);
+            continue;
+          }
+
+          const count = wordFromDb.count + word.count;
+          await dataAccessLayer.update('words', {
+            ...wordFromDb,
+            count,
+          });
+        }
+      });
+
+      response.send({ words, ...book });
+    }
+  );
 
   return router;
 }
